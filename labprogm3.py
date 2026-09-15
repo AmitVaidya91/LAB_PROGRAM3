0a20e26b9429939e22b06758099ac2cc0916ffc6
Obama1 <vaidyaamit2008@gmail.com>
2026-09-15 11:32:28 +0530

    Modify variable assignment to accept user input

--- labprogm3.py
+++ labprogm3.py
@@ -1,7 +1,7 @@
-a = 10 
-b = 20
+a =int(input("Enter the value of a: "))
+b =int(input("Enter the value of b: "))
 print("Before swapping: a =", a, "b =", b)
 temp = a
-a = b 
+a = b  
 b = temp
 print("After swapping: a =", a, "b =", b)
